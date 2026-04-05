f79bfc1b1b5de8fa56660daf0698a1e555967464
Jaime <jsanmar6@epsg.upv.es>
2026-04-05 18:04:25 +0200

    CB-H1-01
    Estructura de carpetas correcta

--- warehouse_navigator/build/warehouse_navigator/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/emilio/turtlebot3_ws/src/warehouse_navigator/install/warehouse_navigator'
