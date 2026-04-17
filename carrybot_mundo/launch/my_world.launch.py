64497fa8476738756e2983a7620e3259d2e0f65e
Emilio <esangra@epsg.upv.es>
2026-04-17 17:08:48 +0200

    Push-corrector
    A algunos miembros del grupo un archivo les aparece dessctualizado.

--- carrybot_mundo/launch/my_world.launch.py
+++ carrybot_mundo/launch/my_world.launch.py
@@ -65,7 +65,7 @@ def generate_launch_description():
     set_env_vars_resources = AppendEnvironmentVariable(
         'GZ_SIM_RESOURCE_PATH',
         os.path.join(
-            get_package_share_directory('my_world'),
+            get_package_share_directory('carrybot_mundo'),
             'models'))
 
     ld = LaunchDescription()
